7085cf0482d8f73af5245a886d0f2b7e9eac8aff
Kristofer Selbekk <selbeezy@gmail.com>
2019-08-22 15:47:52 +0200

    Run create-react-app and delete unnecessary stuff

---
+++ src/data/images.js
@@ -0,0 +1,38 @@
+// Dette er arrayet du skal liste ut
+// Det inneholder bilder på formatet
+// { id: 1, urL: 'http://url.com', description: 'alt-tekst' }
+const images = [
+  {
+    id: 1,
+    url:
+      'https://images.unsplash.com/photo-1556564582-374df0d7577c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description: 'En person som sparker',
+  },
+  {
+    id: 2,
+    url:
+      'https://images.unsplash.com/photo-1556575157-15758d4d0d19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description:
+      'Tre steinmunker som holder seg for henholdsvis ører, munn og øyne',
+  },
+  {
+    id: 3,
+    url:
+      'https://images.unsplash.com/photo-1556595015-dda91ab57740?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description: 'En foss i Yosemite nasjonalpark i USA',
+  },
+  {
+    id: 4,
+    url:
+      'https://images.unsplash.com/photo-1556609516-87806077156a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description: 'To asiatiske ungdommer som står på en stor saltslette',
+  },
+  {
+    id: 5,
+    url:
+      'https://images.unsplash.com/photo-1556560024-b4e093c161a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    description: 'Bagasjerommet til en klassisk Volvo',
+  },
+];
+
+export default images;
